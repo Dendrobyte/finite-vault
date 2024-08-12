3edf1473f82532a67c0b10ef40c3d3beb12ae07e
Dendrobyte <markbacon78@gmail.com>
2024-08-12 14:50:37 -0700

    todos in order

--- backend-go/finite_vault.go
+++ backend-go/finite_vault.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+/*
+ * Shit that needs to get done
+ * [ ] 0. Choose a framework (e.g. fasthttp)
+ * [ ] 1. Set up the OAuth stuff as mirrored in the JS side of things. Auth guards??
+ * [ ] 2. Get the login data working, which means pulling data from mongo and sending it back
+ * [ ] 3. Incrementing daily value when user logs in based on timestamp (this is beyond the JS stuff)
+ * [ ] 4. Full transaction support via mongo, etc. (the actual meaty part, not sure if it'll take longer than auth)
+ */
+
 func main() {
 	fmt.Println("Server starting on port 5000")
 
