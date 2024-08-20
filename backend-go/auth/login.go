ebedf78e6e8556b85dda15fc89d5f77c80796956
Dendrobyte <markbacon78@gmail.com>
2024-08-20 16:15:16 -0700

    Add JWT expiry to golang backend

--- backend-go/auth/login.go
+++ backend-go/auth/login.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/Dendrobyte/finite_vault/db"
 	"github.com/go-chi/chi/v5"
@@ -17,9 +18,16 @@ import (
 
 // TODO: Adding google auth credentials
 var (
-	SIMPLE_LOGIN_CLIENT_ID     string = ""
-	SIMPLE_LOGIN_CLIENT_SECRET string = ""
-	JWT_KEY                    []byte = []byte{}
+	SIMPLE_LOGIN_CLIENT_ID     string
+	SIMPLE_LOGIN_CLIENT_SECRET string
+	JWT_KEY                    []byte
+	EXPIRY_SECONDS             int32
+)
+
+var (
+	ErrInvalidEmail = errors.New("invalid email")
+	ErrInvalidToken = errors.New("the provided token is invalid due for formatting (or it is missing a timestamp field)")
+	ErrTokenExpired = errors.New("token expired")
 )
 
 func init() {
@@ -30,12 +38,14 @@ func init() {
 	SIMPLE_LOGIN_CLIENT_ID = os.Getenv("SIMPLE_LOGIN_CLIENT_ID")
 	SIMPLE_LOGIN_CLIENT_SECRET = os.Getenv("SIMPLE_LOGIN_CLIENT_SECRET")
 	JWT_KEY = []byte(os.Getenv("JWT_KEY"))
+	// TODO: EXPIRY_SECONDS = os.Getenv("EXPIRY_SECONDS")
+	EXPIRY_SECONDS = 604800 // Default to one week
 
 	fmt.Println("-+- Auth module finished loading -+-")
 }
 
 /*
- * Functions to handle login authentication, namely OAuth callback functions and generation of web tokens
+ * Functions to handle login authentication, namely OAuth callback functions and generation/validation of web tokens
  */
 
 // Main entry point for the /login route
@@ -66,57 +76,19 @@ type UserInfo struct {
 	Balance   float32 `json:"balance"`
 }
 
-// Payload for JWT information
-// Information from the token is used for database access for security reasons
-// Token should be in every request sent by the client
-type JWTPayload struct {
-	Email string
-}
-
 // Locally create a JWT with the email encoded in the token
-// TODO: Token expiry? Include that in claims and verify timestamp later
 func createJWT(email string) (s string, err error) {
+	expiryTs := time.Now().Unix() + int64(EXPIRY_SECONDS)
 	var t *jwt.Token = jwt.NewWithClaims(jwt.SigningMethodHS256,
 		jwt.MapClaims{
 			"email": email,
-			"exp":   "2h", // TODO: Make this current time + 2h or something configured as a constant
+			"exp":   expiryTs, // TODO: Make this current time + 2h or something configured as a constant
 		})
 	s, err = t.SignedString(JWT_KEY)
 	if err != nil {
 		fmt.Printf("JWT creation encountered an issue: %v\n", err)
 		return "", errors.New("could not properly sign the created JWT with a key")
 	}
-	return
-}
-
-// Given a token, verify its validity
-// If valid, it will return true and the user's email as a string
-// If not valid, it will return false
-// Error will only not be nil if something goes wrong with processing, and will default to false
-func VerifyJWT(tokenString string) (valid bool, email string, err error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return JWT_KEY, nil
-	})
-
-	if err != nil {
-		return
-	}
-
-	// TODO: Validate the "exp" claim to make sure token is not expired
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		valid = true
-		if e, ok_string := claims["email"].(string); !ok_string {
-			err = errors.New("the email in the token is not valid")
-		} else {
-			email = e
-			valid = true
-		}
-	}
-
 	return
 }
 
@@ -165,4 +137,84 @@ func LoginProton(token string, redirect_uri string) UserInfo {
 
 	log.Printf("Logged in user %v (name: %v)", data.UserData.Email, data.UserData.Name)
 	return UserInfo{userData.Email, jwt, userData.Name, userData.Balance}
+}
+
+// TODO: Maybe it's better to encode this another way, or with a different name?
+type TokenValidationResponse struct {
+	Valid bool
+	Email string
+}
+
+// Given a token, verify its validity
+// If valid, it will return true and the user's email as a string
+// If not valid, it will return false
+// Error will only not be nil if something goes wrong with processing, and will default to false
+func VerifyJWT(tokenString string) (valid bool, email string, err error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return JWT_KEY, nil
+	})
+
+	if err != nil {
+		return
+	}
+
+	// Validate the token claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		// Assume token is valid
+		valid = true
+
+		// Ensure we have a valid email
+		if e, ok_c := claims["email"].(string); !ok_c {
+			err = ErrInvalidEmail
+			valid = false
+			return
+		} else {
+			email = e
+		}
+
+		// Validate expiration claim
+		// Ends up saving in JWT as a 1.7e float64
+		if ts, ok_c := claims["exp"].(float64); !ok_c {
+			err = ErrInvalidToken
+		} else {
+			// Technically, creation and verification always happens on the same system, so timezone isn't important for JWT validation
+			now := time.Now().Unix()
+			if now > int64(ts) {
+				valid = false
+				err = ErrTokenExpired
+				return
+			}
+		}
+
+	}
+
+	return
+}
+
+// Validate the JWT token sent along in the request
+// Will return email so that we don't rely on the user providing an email (which could be falsified)
+func ValidateJWT(w http.ResponseWriter, r *http.Request) {
+	token := r.FormValue("auth_token")
+	valid, email, err := VerifyJWT(token)
+	if err != nil {
+		log.Printf("Error attempting to validate a token for user with email %v: %v\n", email, err)
+		if errors.Is(err, ErrInvalidEmail) {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+		} else if errors.Is(err, ErrInvalidToken) || errors.Is(err, ErrTokenExpired) {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+
+	}
+
+	json.NewEncoder(w).Encode(TokenValidationResponse{valid, email})
+}
+
+// Not for prod, creates for a fake email. Won't do anything otherwise
+func TestCreateJWT(w http.ResponseWriter, r *http.Request) {
+	token, _ := createJWT("mark@mark.mark")
+	json.NewEncoder(w).Encode(token + " expires on " + fmt.Sprint(time.Unix(time.Now().Unix()+int64(EXPIRY_SECONDS), 0)))
 }
