58c141d7f6ebef48ba9823932b4bee2898c920d1
Dendrobyte <markbacon78@gmail.com>
2024-08-30 12:41:12 -0700

    All origins to test Coolify

--- backend-go/finite_vault.go
+++ backend-go/finite_vault.go
@@ -39,7 +39,7 @@ func main() {
 
 	// Middleware (TODO: Authentication)
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "http://10.0.0.*"},
+		AllowedOrigins:   []string{"*"}, //[]string{"http://localhost:5173", "http://10.0.0.*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
